d983c0e8041783ce60287f1f44103fe65a014f93
elvis <cenelvis@gmail.com>
2022-06-06 05:13:25 -0400

    Handling HTTP err, Fetching data with useEffect()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,22 +6,59 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  // useCallBack() only runs when one of its dependencies update
+  // => To avoid infnite loop with useEffect()
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }, []);
+
+  // Fetch data automatically at the end of event cycle & []
+  // NOTE => for best practice we added [fetchMoviesHandler] instead of an empthy []
+  // then => useCallBack() To avoid infnite loop with useEffect()
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  // Default message
+  let content = <p>Found no movies.</p>;
+
+  // Success message
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  // Error message
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  // Loading messsage
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -29,11 +66,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
